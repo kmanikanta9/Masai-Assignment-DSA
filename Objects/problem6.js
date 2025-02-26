d6acc747d970fa426985e042bc45d9cc0e0fe40d
MANIKANTA <manimanikanta41150@gmail.com>
2025-02-26 16:13:44 +0530

    object problem7

--- Objects/problem6.js
+++ Objects/problem6.js
@@ -1,4 +1,4 @@
-let book = { title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 };
+let book = {title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 };
 for(let key in book){
     console.log(key+": "+book[key])
 }
