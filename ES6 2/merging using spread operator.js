111319bec3ffca98301a2afcd3e49b8ab3b01cca
MANIKANTA <manimanikanta41150@gmail.com>
2025-03-06 22:15:43 +0530

    merge and update

---
+++ ES6 2/merging using spread operator.js
@@ -0,0 +1,7 @@
+const profile = { name: "Charlie", age: 29, address: { city: "San Francisco", zipcode: "94101" } };
+
+const updates = { age: 30, address: { zipcode: "94109", country: "USA" } };
+updates.address={...profile.address,...updates.address}
+
+let merged={...profile, ...updates}
+console.log(merged)
